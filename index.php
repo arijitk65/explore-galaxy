292dde946da1ccdf6a93d6d36e989291760a8c08
arijitk65 <39990781+arijitk65@users.noreply.github.com>
2019-09-21 01:49:15 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,93 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="/docs/4.0/assets/img/favicons/favicon.ico">
+
+    <title>My Design</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.0/examples/starter-template/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/style.css" rel="stylesheet">
+
+     <!-- Animations for this template -->
+    <link href="css/animate.css" rel="stylesheet">
+
+    <!-- Google Fonts -->
+    <!-- Welcome text in front-->
+    <link href="https://fonts.googleapis.com/css?family=Mansalva&display=swap" rel="stylesheet">
+
+
+  </head>
+
+ 
+
+  <body>
+
+    
+
+    <?php include('includes/navbar.php'); ?>
+
+    <main>
+
+     <section id="hp-01">
+       <div class="container-fluid">
+         <div class="row">
+           <div class="col-md-12">
+             <h3 class="welcome-text">WELCOME TO THIS UNIVERSE</h3>
+           </div>
+           <a href="#hp-02"><img class="animated bounce infinite" src="images/icons/down_arrow.png"></a>
+         </div>
+       </div>
+     </section>
+
+     <section id="hp-02">
+       <div class="container-fluid">
+         <div class="row">
+            <video autoplay loop muted>
+              <source src="videos/galaxy_01.mp4" type="video/mp4" />
+              <source src="videos/galaxy_01.ogg" type="video/ogg" />
+            </video>
+            <div class="col-lg-4 col-md-6 col-sm-10 col-xm-10 card">
+              <div class="row p-3">
+                <div class="col-12">
+                  <h6>Hello World</h6>
+                </div>
+                <div class="col-12">
+                  <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>
+                </div>
+                <div class="col-4">
+                   <a href="#" class="btn btn-info btn-sm">Know More</a>
+                </div>
+              </div><!-- .row -->
+              
+              <div>
+                
+              </div>
+            </div>
+           
+           
+         </div>
+       </div>
+     </section>
+
+    </main><!-- /.container -->
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script>window.jQuery || document.write('<script src="../../assets/js/vendor/jquery-slim.min.js"><\/script>')</script>
+    <script src="js/vendor/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
